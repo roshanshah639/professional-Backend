840d74f0e511b4d4809e95716ecd67cecaa486c3
Roshan Shah <roshanshah639@gmail.com>
2023-11-27 13:03:20 +0545

    utlis code discussion

--- src/index.js
+++ src/index.js
@@ -6,25 +6,13 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
-/* import express from "express";
-const app = express();
-
-async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log("MongoDB connected");
-    app.on("error", (error) => {
-      console.error("ERROR:", error);
-      throw error;
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
     });
+  })
 
-    app.listen(process.env.PORT, () => {
-      console.log(`Listening on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("ERROR:", error);
-    throw error;
-  }
-};
- */
+  .catch((error) => {
+    console.log("MongoDB connection failed", error);
+  });
